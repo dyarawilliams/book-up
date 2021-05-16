b77dd233b4d9c714120fe1b57dc7b3178dc40707
D'yara Williams <dyara.williams@gmail.com>
2021-05-16 17:33:15 -0400

    added semicolons

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const connectDB = require("./config/database");
-
 const mainRouter = require('./routes/main')
 const authorRouter = require('./routes/authors')
 
@@ -12,23 +11,22 @@ const authorRouter = require('./routes/authors')
 require("dotenv").config({ path: "./config/.env" });
 
 // Connect to Database
-connectDB()
+connectDB();
 
 
 // Using ejs for views
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
 
 // Static Folder
-app.use(express.static('public'))
-
+app.use(express.static('public'));
 
 app.use('/', mainRouter)
-app.use('/authors', authorRouter)
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 8000,  () => {
-    console.log(`Server is running, you better catch it!`)
+    console.log(`Server is running, you better catch it!`);
 })
