bcf89308263717ff924cc303ec5ff69b73edbf1e
D'yara Williams <dyara.williams@gmail.com>
2021-05-16 20:04:18 -0400

    added body parser

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
 const connectDB = require("./config/database");
 const mainRouter = require('./routes/main')
 const authorRouter = require('./routes/authors')
@@ -13,12 +15,11 @@ require("dotenv").config({ path: "./config/.env" });
 // Connect to Database
 connectDB();
 
-
-// Using ejs for views
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // Static Folder
 app.use(express.static('public'));
