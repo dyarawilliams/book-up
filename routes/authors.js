482a0ed6e46cd279cb4582b4ab7ca20842d633b0
D'yara Williams <dyara.williams@gmail.com>
2021-05-16 20:35:26 -0400

    updated author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All Authors
 router.get('/', (req, res) => {
@@ -8,12 +9,26 @@ router.get('/', (req, res) => {
 
 // New Author
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create Author 
 router.post('/', (req, res) => {
-    res.send('Created new author')
+    const newAuthor = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if(err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else {
+            // res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
+    // res.send(req.body.name)
 })
 
 
