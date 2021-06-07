3b56ca2f25312e7e63fc4c393de1494b1f14f4c5
D'yara Williams <dyara.williams@gmail.com>
2021-06-07 00:50:21 -0400

    working search query implemented

--- routes/authors.js
+++ routes/authors.js
@@ -4,9 +4,16 @@ const Author = require('../models/author')
 
 // All Authors
 router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name != '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
     try {
-        const authors = await Author.find({})
-        res.render('authors/index', { authors: authors })
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query 
+        })
     } catch (err) {
         res.redirect('/')
         console.log(err)
