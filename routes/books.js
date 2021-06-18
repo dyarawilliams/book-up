ce04143d690776bfc27fbea2b90991a5f02b2f45
D'yara Williams <dyara.williams@gmail.com>
2021-06-17 23:31:42 -0400

    publish before and after filters working

--- routes/books.js
+++ routes/books.js
@@ -20,6 +20,14 @@ router.get('/', async (req, res) => {
     if (req.query.title != null && req.query.title != '') {
         query = query.regex('title', new RegExp(req.query.title, 'i'))
     }
+    // Publish Before filter
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query .lte('publishDate', req.query.publishedBefore)
+    }
+    // Publish After filter
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query .gte('publishDate', req.query.publishedAfter)
+    }
     try {
         const books = await query.exec()
         res.render('books/index', {
