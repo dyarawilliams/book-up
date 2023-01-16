98f7e50893db62e23c660321ef2d9dff4c69fc7f
D'yara Williams <dyara.williams@gmail.com>
2023-01-15 22:46:08 -0500

    update packages, and add strictQuery true

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    mongoose.set('strictQuery', true);
     const conn = await mongoose.connect(process.env.DB_STRING);
+    
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
