d331729c26f6f6970a39299c151f547a88067be2
D'yara Williams <dyara.williams@gmail.com>
2024-03-12 23:55:15 -0400

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
-const myCarouselElement = document.querySelector('#carouselExampleCaptions')
+// const myCarouselElement = document.querySelector('#carouselExampleCaptions')
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: 2000,
-    wrap: false
-})
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//     interval: 2000,
+//     wrap: false
+// })
+
+const hamBurger = document.querySelector(".toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
