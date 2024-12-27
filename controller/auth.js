6f1ff0094d22495b6b2e0df955ce0ac580d2643c
D'yara Williams <dyara.williams@gmail.com>
2024-12-27 14:14:05 -0500

    fix:typo error is session destory for logout

--- controller/auth.js
+++ controller/auth.js
@@ -48,7 +48,7 @@ module.exports = {
                 if (err) { return next(err); }
                 console.log('User has logged out.')
             })
-            req.session.destory((err) => {
+            req.session.destroy((err) => {
                 if (err) {
                     console.log('Error : Failed to destroy the session during logout.', err)
                 } else {
