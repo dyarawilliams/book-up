d2f08d1e816f09b8734ba87f52108e9639df6d6a
D'yara Williams <dyara.williams@gmail.com>
2024-12-28 01:48:20 -0500

    fix: login and logout routes

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(session({
         saveUninitialized: true,
         store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
         cookie: { 
-            maxAge: 1800000 // 30 minutes in milliseconds
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
         }
     })
 )
